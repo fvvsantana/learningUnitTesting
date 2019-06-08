1db62086cfe0e1d6b87c067ecb93e7be76e36c25
Flavio Santana <fvvsantana@gmail.com>
2019-06-08 16:09:28 -0300

    Add main test and delete pacman.ser

---
+++ src/pactest/MainMain.java
@@ -0,0 +1,16 @@
+package pactest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import pacman.Main;
+
+class MainMain {
+
+	@Test
+	void test() {
+		Main.main(null);
+	}
+
+}
